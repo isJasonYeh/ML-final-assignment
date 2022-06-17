77f15d801a62bc85861c4f0eae8828311cac2c6e
isJasonYeh <isjasonyeh@gmail.com>
2022-06-17 06:16:08 +0000

    3th commit

--- yolov4/.ipynb_checkpoints/download-checkpoint.py
+++
@@ -1,3 +0,0 @@
-!wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.conv.29
-!whet https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.conv.137
-print("Download weights done.")
