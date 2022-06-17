4472fc6d5d495526c00fed52d5208a85641b9b62
isJasonYeh <isjasonyeh@gmail.com>
2022-06-17 06:35:28 +0000

    3th commit

---
+++ yolov4/download-tiny.sh
@@ -0,0 +1,2 @@
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.conv.29
+echo "Download weights done."
---
+++ yolov4/download.sh
@@ -0,0 +1,2 @@
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.conv.137
+echo "Download weights done."
