19826f85a3c393224cdf2dbde5786aa2578cec06
isJasonYeh <isjasonyeh@gmail.com>
2022-06-17 07:19:23 +0000

    add train

--- yolov4/makeList.py
+++ yolov4/makeList.py
@@ -4,14 +4,14 @@ import os
 probability = 0.8
 #train資料佔的比例，valid為1-probability
 
-dir = '/root/stove_switch/'
+dir = '/root/ML-final-assignment/yolov4/stove_switch'
 
 fileList =[f for f in os.listdir(dir) if f.endswith('.jpg')]
 #使用os.listdir取得該目錄下的所有檔案名稱，並且用endswith()判斷後面的字(附檔名)，為True的才存到Tuple裡面
 
 random.shuffle(fileList) #打亂順序
 
-with open('/root/Final/yolov4/train.txt', 'w') as train, open('/root/Final/yolov4/train.txt', 'w') as valid:
+with open('/root/ML-final-assignment/yolov4/train.txt', 'w') as train, open('/root/ML-final-assignment/yolov4/valid.txt', 'w') as valid:
   for fileName in fileList:
     if (random.random() <= probability):
       train.write(dir + fileName + "\n")
